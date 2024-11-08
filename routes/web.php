4a3dd187c2e7187f98a89b81322660e4ee849fea
unknown <vereladitya07@gmail.com>
2024-11-08 23:42:42 +0800

    nambah halaman user

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,11 @@
 use App\Http\Controllers\BooksController;
 use App\Http\Controllers\CategoriesController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::prefix('admin')->group(function()
 {
@@ -15,7 +16,8 @@ Route::prefix('admin')->group(function()
     Route::post('/books', [BooksController::class, 'storeBooks']) ->name('books.store');
     Route::delete('/books/{book_id}', [BooksController::class, 'delBooks']) -> name('books.delete');  
     Route::put('/books/{book_id}', [BooksController::class, 'updateBooks']) -> name('books.update');  
-
     Route::delete('/{category_id}', [CategoriesController::class, 'delCategories']) -> name('categories.delete');
     Route::post('/categories', [CategoriesController::class, 'storeCategories']) -> name('categories.store');
 });
+
+Route::get('/', [UserController::class, 'index']);
