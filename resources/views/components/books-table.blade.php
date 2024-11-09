658fc9217b918b3166d021244cec8a4bdfd7d522
CherunoBread <qiyaanugrah75@gmail.com>
2024-11-09 11:56:35 +0700

    handling filter dan stock

--- resources/views/components/books-table.blade.php
+++ resources/views/components/books-table.blade.php
@@ -115,7 +115,7 @@
                                 <div class="col-span-2 sm:col-span-1">
                                     <label for="stock"
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Stock</label>
-                                    <input type="number" name="stock" id="stock"
+                                    <input type="number" name="stock" id="stock" min='0'
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                                         required="">
                                 </div>
