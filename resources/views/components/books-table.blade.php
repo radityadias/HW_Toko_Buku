51e5d62d62423227055e0cd54285764d57741e6d
unknown <vereladitya07@gmail.com>
2024-11-09 22:06:13 +0800

    memperbaiki tombol konfirm edit

--- resources/views/components/books-table.blade.php
+++ resources/views/components/books-table.blade.php
@@ -435,7 +435,7 @@
                                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                                             <select id="category_id" name="category_id"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                                <option disabled selected="" value="{{ $item->category_id }}">{{$item->category->name}}</option>
+                                                <option selected="" value="{{ $item->category_id }}">{{$item->category->name}}</option>
                                                 @foreach ($categories as $category)
                                                     <option value="{{ $category->category_id }}">
                                                         {{ $category->name }}</option>
@@ -446,13 +446,7 @@
                                     </div>
                                     <button type="submit"
                                         class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                        <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                                            xmlns="http://www.w3.org/2000/svg">
-                                            <path fill-rule="evenodd"
-                                                d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                                                clip-rule="evenodd"></path>
-                                        </svg>
-                                        Add new product
+                                        Save
                                     </button>
                                 </form>
                             </div>
