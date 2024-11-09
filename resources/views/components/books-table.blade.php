71e38d055bab97eec63df722361a38b2967c9a4d
CherunoBread <qiyaanugrah75@gmail.com>
2024-11-09 19:29:34 +0700

    fix bug edit

--- resources/views/components/books-table.blade.php
+++ resources/views/components/books-table.blade.php
@@ -435,7 +435,7 @@
                                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
                                             <select id="category_id" name="category_id"
                                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                                <option disabled selected="" value="{{ $item->category_id }}">{{$item->category->name}}</option>
+                                                <option selected="" value="{{ $item->category_id }}">{{$item->category->name}}</option>
                                                 @foreach ($categories as $category)
                                                     <option value="{{ $category->category_id }}">
                                                         {{ $category->name }}</option>
